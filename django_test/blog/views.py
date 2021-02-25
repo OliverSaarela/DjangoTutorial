63f24bdfe8a13ea34fb35aa31c7b9daf96f40c01
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2021-02-25 17:00:49 +0200

    Part 5 done

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -1,31 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2020'
-    },
-    {
-        'author': 'James',
-        'title': 'Blog Post 2',
-        'content': 'second post content',
-        'date_posted': 'August 28, 2020'
-    },
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'August 28, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
